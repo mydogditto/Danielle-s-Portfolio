28b8d97d424b85335925f87b78b72f9813294f36
mydogditto <dhgoldberg15@gmail.com>
2025-08-10 16:21:04 -0400

    ADD setup files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ fontFamily: "'Lato', sans-serif", backgroundColor: '#F2E4D7', minHeight: '100vh', padding: '2rem', color: '#2E2E2E' }}>
+      <h1 style={{ fontFamily: "'Limelight", fontSize: '3rem', marginBottom: '1rem' }}>
+        Danielle Goldberg Portfolio
+      </h1>
+      <p>Welcome to my portfolio site. Here you will find my app and print design work.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'server', 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    historyApiFallback: true,
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+};
